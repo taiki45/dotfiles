5a38d7fdf774a93f722f5595afd046ee9eb4306e
Taiki Ono <taiks.4559@gmail.com>
2019-07-15 23:05:06 +0900

    google chrome
    Signed-off-by: Taiki Ono <taiks.4559@gmail.com>

--- xremap_config.rb
+++ xremap_config.rb
@@ -21,3 +21,27 @@ window class_only: 'chromium-browser' do
   remap 'M-d', to: 'Ctrl-Delete'
   remap 'C-h', to: 'BackSpace'
 end
+
+window class_only: 'google-chrome' do
+  %w[f l w t n y c v r equal minus 0 a x z].each do |key|
+    remap "Super-#{key}", to: "C-#{key}"
+  end
+
+  remap "Super-Shift-bracketright", to: "Ctrl-Tab"
+  remap "Super-Shift-bracketleft", to: "Ctrl-Shift-Tab"
+
+  remap 'C-b', to: 'Left'
+  remap 'C-f', to: 'Right'
+  remap 'C-p', to: 'Up'
+  remap 'C-n', to: 'Down'
+
+  remap 'M-b', to: 'Ctrl-Left'
+  remap 'M-f', to: 'Ctrl-Right'
+
+  remap 'C-a', to: 'Home'
+  remap 'C-e', to: 'End'
+
+  remap 'C-d', to: 'Delete'
+  remap 'M-d', to: 'Ctrl-Delete'
+  remap 'C-h', to: 'BackSpace'
+end
